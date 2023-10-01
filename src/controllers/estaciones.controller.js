94c7776c14a4b3b23792a258ac545da52a2a16af
Aldahir Mayorga <samg.25sep@gmail.com>
2023-10-01 16:26:08 -0600

    Actualizando consulta SQL

--- src/controllers/estaciones.controller.js
+++ src/controllers/estaciones.controller.js
@@ -5,10 +5,10 @@ const getEstacionByID = async (req, res) => {
     try {
         const id = req.params.id;
 
-        //const [rows] = await pool.query('SELECT * FROM stations_brands where id = ?', [id]);
-        const [rows] = await pool.query('SELECT sb.id, s.name from stations_brands as sb inner join stations as s on sb.cre_id = s.cre_id where sb.id = ?', [id]);
+
+        const [rows] = await pool.query('SELECT sb.id, s.name ,sc.distance, p.value, b.name FROM stations_brands AS sb INNER JOIN stations AS s on sb.cre_id = s.cre_id INNER JOIN stations_competitors AS sc on sc.cre_id = s.cre_id INNER JOIN prices AS p on p.cre_id = s.cre_id INNER JOIN brands AS b on b.id = sb.id_brand WHERE sb.id = ?', [id]);
         if (rows.length < 1) return res.status(404).json({ "message": 'Station not found' });
-        res.json(rows);
+        res.json(rows[0]);
     } catch (error) {
         return res.status(500).json({ "message": "Something goes wrong" })
     }
